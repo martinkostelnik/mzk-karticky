56b458e31c900a81a66c569d13c616d6404ac408
Faz0lek <martinkostelnik0@gmail.com>
2022-10-06 12:07:37 +0200

    Update inference with json containing model config and remove confidence for now

--- src/NER/inference.py
+++ src/NER/inference.py
@@ -2,22 +2,21 @@ import os
 import typing
 import argparse
 import numpy as np
+import torch
+
+import helper
 
 from model import build_model
-from helper import IDS2LABELS, MAX_TOKENS_LEN, NUM_LABELS, calculate_confidence
-from typing import Callable
-
-import torch
-from transformers import BertTokenizerFast
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--data-path", help="Path to a directory containing data for inference.")
-    parser.add_argument("--model-path", help="Path to a trained model.")
-    parser.add_argument("--tokenizer-path", help="Path to a tokenizer.")
-    parser.add_argument("--save-path", help="Path to a output directory.")
+    parser.add_argument("--data-path", help="Path to a directory containing data for inference.", required=True)
+    parser.add_argument("--config-path", help="Path to a json file contatining model config.", required=True)
+    parser.add_argument("--model-path", help="Path to a trained model.", required=True)
+    parser.add_argument("--tokenizer-path", help="Path to a tokenizer.", required=True)
+    parser.add_argument("--save-path", help="Path to a output directory.", required=True)
     
     parser.add_argument("--threshold", default=0.0, type=float, help="Threshold for selecting field with enough model confidence.")
     parser.add_argument("--aggfunc", default="prod", type=str, help="Confidence aggregation function.")
@@ -26,7 +25,7 @@ def parse_arguments():
     return args
 
 
-def get_aggfunc(func_name: str) -> Callable:
+def get_aggfunc(func_name: str) -> typing.Callable:
     if func_name == "prod":
         return np.prod
 
@@ -48,31 +47,35 @@ def get_ocr(file_path: str) -> str:
         return f.read()
 
 
-def logits_to_preds(tokenizer, logits, ids, offset_mapping) -> list:
-    active_logits = logits.view(-1, NUM_LABELS)
+def logits_to_preds(model_config, tokenizer, logits, ids, offset_mapping, ocr) -> list:
+    active_logits = logits.view(-1, model_config.num_labels)
     flattened_predictions = torch.argmax(active_logits, axis=1)
 
     wordpieces = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
-    wordpieces_preds = [IDS2LABELS[pred] for pred in flattened_predictions.cpu().numpy()]
+    wordpieces_preds = [model_config.ids2labels[pred] for pred in flattened_predictions.cpu().numpy()]
 
     result = []
 
-    for pred, mapping in zip(wordpieces_preds, offset_mapping.squeeze().tolist()):
-        if mapping[0] == 0 and mapping[1] != 0:
+    i = 0
+    for tok, pred, mapping in zip(wordpieces, wordpieces_preds, offset_mapping.squeeze().tolist()):
+        if mapping[0] == 0 and mapping[1] != 0 and tok != "[SEP]":
             result.append(pred)
+            # print(pred, tok, ocr.split()[i])
+            i += 1
 
     return result
 
 
-def infer(ocr: str, tokenizer, model) -> list:
-    words = ocr.split()
+def infer(ocr: str, tokenizer, model, filename) -> list:
+    words = ocr.replace("\n", " [SEP] ").split()
+    asdf = len([word for word in words if word == "[SEP]"])
 
     encoding = tokenizer(words,
                          padding="max_length",
                          is_split_into_words=True,
                          return_offsets_mapping=True,
                          truncation=True,
-                         max_length=MAX_TOKENS_LEN,
+                         max_length=model.config.max_sequence_len,
                          return_tensors="pt")
 
     device = model.get_device()
@@ -83,10 +86,11 @@ def infer(ocr: str, tokenizer, model) -> list:
     with torch.no_grad():
         logits = model(ids, attention_mask=mask)[1][0]
 
-    preds = logits_to_preds(tokenizer, logits, ids, encoding["offset_mapping"])
-    confidence = calculate_confidence(logits)[1:len(preds) + 1].tolist()
+    preds = logits_to_preds(model.config, tokenizer, logits, ids, encoding["offset_mapping"], ocr)
 
-    return list(zip(words, preds, confidence))
+    confidence = helper.calculate_confidence(logits)[1:len(preds) + 1].tolist()
+
+    return list(zip(ocr.split(), preds, confidence))
 
 
 def find_offsets(text: str, data: list) -> list:
@@ -128,7 +132,7 @@ def concat_token_offsets(offsets: list) -> list:
     return result
 
 
-def aggregate_confidence(alignments: list, aggfunc: Callable) -> list:
+def aggregate_confidence(alignments: list, aggfunc: typing.Callable) -> list:
     result = []
 
     for label, from_, to, confidences in alignments:
@@ -138,9 +142,6 @@ def aggregate_confidence(alignments: list, aggfunc: Callable) -> list:
 
 
 def save_result(path: str, result: list, ocr: str) -> None:
-    output_folder = path.rpartition("/")[0]
-    os.makedirs(output_folder, exist_ok=True)
-
     with open(path, "w") as f:
         for label, from_, to, _ in result:
             print(f"{label}\t{repr(ocr[from_:to])}\t{from_}\t{to}", file=f)
@@ -156,20 +157,29 @@ def main() -> int:
     args = parse_arguments()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
 
-    model = build_model(model_path=args.model_path)
+    model_config = helper.ModelConfig.load(args.config_path)
+    print("Model config loaded.")
+
+    tokenizer = helper.build_tokenizer(args.tokenizer_path, model_config)
+    print("Tokenizer loaded.")
+
+    model = build_model(tokenizer=tokenizer, model_path=args.model_path, model_config=model_config)
     model = model.to(device)
     model.eval()
-
-    tokenizer = BertTokenizerFast.from_pretrained(args.tokenizer_path)
+    print("Model loaded.")
 
     output_dataset = []
 
     print(f"Aggregation function: {args.aggfunc}")
     print(f"Confidence threshold: {args.threshold}")
     aggfunc = get_aggfunc(args.aggfunc)
-    total = 0
 
+    os.makedirs(args.save_path, exist_ok=True)
+    print("Output directory created.")
+
+    print("Inference starting ...")
     for root, _, filenames in os.walk(args.data_path):
         for filename in filenames:
             file_path = os.path.join(root, filename)
@@ -178,25 +188,27 @@ def main() -> int:
 
             ocr = get_ocr(file_path)
             
-            result = infer(ocr, tokenizer, model)
+            result = infer(ocr, tokenizer, model, filename)
 
             token_offsets = find_offsets(ocr, result)
             alignments = concat_token_offsets(token_offsets)
             alignments = [alignment for alignment in alignments if alignment[0] != "O"]
         
-            alignments = aggregate_confidence(alignments, aggfunc)
-            alignments = [alignment for alignment in alignments if alignment[3] >= args.threshold]
+            # alignments = aggregate_confidence(alignments, aggfunc)
+            # alignments = [alignment for alignment in alignments if alignment[3] >= args.threshold]
 
             for label, from_, to, _ in alignments:
                 line += f"\t{label} {from_} {to}"
-                total += 1
 
             output_dataset.append(line)
 
             save_result(os.path.join(args.save_path, filename), alignments, ocr)
 
+    print("Inference done.")
+
     save_output_dataset(output_dataset, args.save_path)
-    
+    print("Output dataset saved.")
+
     return 0
 
 
