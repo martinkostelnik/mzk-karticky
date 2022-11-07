96bc4ff963e611e495a32ec780f7d859308c73c9
Faz0lek <martinkostelnik0@gmail.com>
2022-11-07 12:54:15 +0100

    Add added files counter

--- src/NER/create_lmdb.py
+++ src/NER/create_lmdb.py
@@ -27,6 +27,8 @@ def main():
     args = parse_arguments()
 
     gb100 = 100000000000
+    n_files = 0
+
     env = lmdb.open(args.lmdb, map_size=gb100)
     with env.begin(write=True) as txn:
         cur = txn.cursor()
@@ -44,6 +46,12 @@ def main():
                     file_id = file_id[1:]
 
                 write(cur, file_id, text)
+                n_files += 1
+
+                if n_files % 1000 == 0:
+                    print(f"{n_files} files added to db.")
+
+    print(f"{n_files} files added to db.")
 
     return 0
 
